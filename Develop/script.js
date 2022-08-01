08fe11b354c70f7a9942098c54ff561ff52da4de
Jared Love <lovejared91@yahoo.com>
2022-08-01 17:38:22 -0500

    added to generatepassword function

--- Develop/script.js
+++ Develop/script.js
@@ -8,12 +8,30 @@ var pick;
 //"\"
 special = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}", "~", "(", ")", "_"]
 numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 function generatePassword() {
+  var length = window.prompt("Choose a password length between 8 and 128")
+    if (!length) {
+      alert("Please choose a correct length between 8 and 128")
+      return generatePassword()
+    }
+
+    else if (length < 8 || length > 128) {
+      alert("The password length must be between 8 and 128 characters")
+      return generatePassword()
+    }
+
+    else {
+    promtLowerCase = confirm("Will this password use lowercase letters?")
+    promtUpperCase = confirm("Will this password use Uowercase letters?")
+    promtNumeric = confirm("Will this password use numbers?")
+    promtSpecial = confirm("Will this password contain special letters?")
+    }
 
  
-  return("pasword")
-}
+  //return("password")
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
