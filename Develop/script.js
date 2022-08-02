e009a0656b29ef4a771d0dd1ccd270e886d8039b
Jared Love <lovejared91@yahoo.com>
2022-08-02 09:20:59 -0500

    Add if and if else statements

--- Develop/script.js
+++ Develop/script.js
@@ -4,16 +4,17 @@ var promtUpperCase;
 var promtNumeric;
 var promtSpecial;
 var promtLength;
-var pick;
+
 //"\"
 special = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}", "~", "(", ")", "_"]
 numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 function generatePassword() {
-  var length = window.prompt("Choose a password length between 8 and 128")
-    if (!length) {
-      alert("Please choose a correct length between 8 and 128")
+  var length = window.prompt("Choose a password length between 8 and 128")  
+  
+  if (!length) {
+      alert("Choose a correct length between 8 and 128")
       return generatePassword()
     }
 
@@ -27,9 +28,71 @@ function generatePassword() {
     promtUpperCase = confirm("Will this password use Uowercase letters?")
     promtNumeric = confirm("Will this password use numbers?")
     promtSpecial = confirm("Will this password contain special letters?")
+    };
+   
+    if (!promtLowerCase && !promtUpperCase && !promtNumeric && !promtSpecial) {
+      picks = alert("Choose atleast one option!")
+      return generatePassword();
     }
 
- 
+    else if (promtLowerCase && promtUpperCase && promtNumeric && promtSpecial) {
+
+    }
+
+    else if (promtLowerCase && promtUpperCase && promtNumeric) {
+      
+    }
+
+    else if (promtLowerCase && promtUpperCase) {
+      
+    }
+
+    else if (promtLowerCase && promtSpecial) {
+      
+    }
+
+    else if (promtLowerCase && promtNumeric) {
+      
+    }
+
+    else if (promtLowerCase) {
+      
+    }
+
+    else if (promtUpperCase && promtNumeric && promtSpecial) {
+
+    }
+
+    else if (promtUpperCase && promtNumeric) {
+
+    }
+
+    else if (promtUpperCase && promtSpecial) {
+
+    }
+
+    else if (promtUpperCase) {
+
+    }
+
+    else if (promtNumeric && promtSpecial) {
+      
+    }
+
+    else if (promtNumeric) {
+
+    }
+
+    else if (promtSpecial) {
+
+    }
+
+    
+
+
+
+      
+    
   //return("password")
 };
 
