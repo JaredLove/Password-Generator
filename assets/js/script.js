63875d5d76d544dcf76b352756f14a443aa1c395
JaredLove <lovejared91@Yahoo.com>
2023-07-07 15:40:27 -0500

    updates to logic and error outputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,233 +1,116 @@
-let lower = document.getElementById("lowerCase");
-let upper = document.getElementById("upperCase");
-let specials = document.getElementById("special");
-let number = document.getElementById("numbers");
-let passLength = document.getElementById("password-length");
-//special characters
-let special = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}", "~", "(", ")", "_"];
-
-//numeric values 
-let numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-//lower case values
-let alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-//upper case values
-let alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
-// variables that will be used
-let picks;
- 
-//start of generatepassword function
-  function generatePassword() {
-  //window promt to get info from user for the length of their password
- console.log(passLength.value);
+let errorCheck = document.getElementById("error");
+let errorCheck2 = document.getElementById("error2");
+let errorCheck3 = document.getElementById("error3");
 
+// Generate random password
+function generatePassword() {
+  let lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  let uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let specialChars = "!@#$%^&*()_-+=[]{}<>,.?/~";
+  let numberChars = "0123456789";
+
+  let password = "";
+  
+  let useLowercase = document.getElementById("lowercase").checked;
+  let useUppercase = document.getElementById("uppercase").checked;
+  let useSpecialChars = document.getElementById("special").checked;
+  let useNumbers = document.getElementById("numbers").checked;
+  
+  let allowedChars = "";
+  
+  if (useLowercase) {
+    allowedChars += lowercaseChars;
+  }
+  
+  if (useUppercase) {
+    allowedChars += uppercaseChars;
     
-    //input validation to make sure it is a number value - isNaN determines whether a value is NaN(not a number) or not
-    if (isNaN(passLength.value)) {
-      alert ("Please enter a number!"); // alert if the input is not a number
-       exit(); // sends you back to the generate function to try again
-    }
+  }
+  
+  if (useSpecialChars) {
+    allowedChars += specialChars;
 
-    
-    //input validation to make sure the value from the user is atleast 8 characters and no more than 128 characters
-    else if (passLength.value < 8 || passLength.value > 128) {
-      alert("The password length must be between 8 and 128 characters"); // alert if the password length is not between 8 and 128 characters
-       exit(); // sends you back to the generate function to try again
-    }
+  }
+  
+  if (useNumbers) {
+    allowedChars += numberChars;
+   
+  }
+  
+  if (allowedChars === "") {
+    errorCheck2.classList.remove("error2");
+    errorCheck2.classList.add("error2-show");
+  }
+  
+  let passwordLengthInput = document.getElementById("password-length");
+  let passwordLength = parseInt(passwordLengthInput.value);
+  
+  if (isNaN(passwordLength)) {
+    errorCheck.classList.remove("error");
+    errorCheck.classList.add("error-show");
 
-    
-    
-    //This will send the user with a dialog box with a message that they can click ok for yes and cancel for no
-    
-    // promtLowerCase = confirm("Will this password use lowercase letters?"); //Ask the user if they would like lowercase letters in their password
-    
-    // promtUpperCase = confirm("Will this password use uppercase letters?");//Ask the user if they would like uppercase letters in their password
+  }
+  
+  if (passwordLength < 8 || passwordLength > 128) {
+    errorCheck3.classList.remove("error3");
+    errorCheck3.classList.add("error3-show");
+  }
 
-    // promtNumeric = confirm("Will this password use numbers?");//Ask the user if they would like numbers in their password
-
-    // promtSpecial = confirm("Will this password contain special letters like @ or #?");//Ask the user if they would like special letters in their password
-    
-    
-    //start of if and if else statements for what the user wanted
-    // validation to make sure the user said yes to atleast one option
-    if (!lower && !upper && !specials && !number) {
-
-      picks = alert("Choose atleast one option!"); //sends a alert to the user that they did not select atleast one option
-
-      return generatePassword(); //sends the user back to the generatePassword funtion to try again
-
-    }
-
-    
-    //if the user slected all 4 options to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (lower && upper && specials && number) {
-
-        picks = alphabetLower.concat(alphabetUpper, numeric, special);
-
-    }
-     
-    
-    //if the user slected lowercase, uppercase, and numeric to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (lower && upper && number) {
-
-      picks = alphabetLower.concat(alphabetUpper, numeric);
-
-    }
-     
-    
-    //if the user slected lowercase and uppercase to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (lower && upper) {
-
-      picks = alphabetLower.concat(alphabetUpper);
-
-    }
-     
-    
-    //if the user slected lowercase and special characters to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (lower && specials) {
-
-      picks = alphabetLower.concat(special);
-
-    }
-     
-    
-    //if the user slected lowercase and numeric to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (lower && number) {
-
-      picks = alphabetLower.concat(numeric);
-
-    }
-     
-    
-    //if the user slected just lowercase to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (lower) {
-
-      picks = alphabetLower;
-
-    }
-     
-    
-    //if the user slected uppercase, numeric, and special characters to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (upper && number && specials) {
-
-      picks = alphabetUpper.concat(numeric, special);
-
-    }
-     
-    
-    //if the user slected uppercase and numeric to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (upper && number) {
-
-      picks = alphabetUpper.concat(numeric);
-
-    }
-     
-    
-    //if the user slected uppercase and special characters to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (upper && specials) {
-
-      picks = alphabetUpper.concat(special);
-
-    }
-     
-    
-    //if the user slected just uppercase lettes to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (upper) {
-
-      picks = alphabetUpper;
-
-    }
-     
-    
-    //if the user slected numeric and special characters  to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (number && specials) {
-
-      picks = numeric.concat(special);
-
-    }
-     
-    
-    //if the user slected just numeric values to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (number) {
-
-      picks = numeric;
-
-    }
-     
-    
-    //if the user slected just special characters to be in their password
-    //.concat will add 2 or more strings together found this in w3schools
-    else if (specials) {
-
-      picks = special;
-
-    };
-
-    
-    
-    //This is an empty array they will hold our length
-    let pass1 = [];
-
-    
-    
-    //for loop the will randomize all of the letaibles for the users password using math floor and math random that will than sends the picks2 letiable using .push to are let pasword
-    for (let i = 0; i < passLength.value; i ++) {
-
-      let picks2 = picks[Math.floor(Math.random() * picks.length)];
-
-      pass1.push(picks2);
-
-    }
-
-    
-    
-    //turns the array into a string that will be sent to the user
-    //got this from w3schools
-    let pass2 = pass1.join("");
-
-
-    //returns the string that will be outputed to user
-    return pass2;
-
-
+  
+  for (let i = 0; i < passwordLength; i++) {
+    let randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars.charAt(randomIndex);
+  }
+  
+  return password;
 }
 
-// Get references to the #generate element
-let generateBtn = document.querySelector("#generate");
-
-
-// Write password to the #password input
-function writePassword() {
-
-  let password = generatePassword();
-
-  let passwordText = document.querySelector("#password");
-
-
-  passwordText.value = password;
-
-
+// Update password field with generated password
+function updatePassword() {
+  let passwordField = document.getElementById("password");
+  passwordField.value = generatePassword();
 }
 
-function exit() {
-  throw new Error("Input is invalid. Execution stopped.");
+function exit() { 
+  throw new Error("invalid input");
 }
 
+let body = document.body;
+let cardDark = document.getElementById("card");
+let h1Dark = document.getElementById("h1");
+function toggleDarkMode() {
+  let darkModeBtn = document.getElementById("dark-mode-btn");
+  let lightModeBtn = document.getElementById("light-mode-btn");
+  
+  document.body.classList.add("dark-mode");
+  cardDark.classList.add("dark-mode");
+  h1Dark.classList.add("dark-mode");
+  
+  darkModeBtn.style.display = "none";
+  lightModeBtn.style.display = "inline-block";
+}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function toggleLightMode() {
+  let darkModeBtn = document.getElementById("dark-mode-btn");
+  let lightModeBtn = document.getElementById("light-mode-btn");
+  
+  document.body.classList.remove("dark-mode");
+  cardDark.classList.remove("dark-mode");
+  h1Dark.classList.remove("dark-mode");
+  darkModeBtn.style.display = "inline-block";
+  lightModeBtn.style.display = "none";
+}
+
+let darkModeBtn = document.getElementById("dark-mode-btn");
+let lightModeBtn = document.getElementById("light-mode-btn");
+
+// Add event listener to the generate button
+let generateBtn = document.getElementById("generate-btn");
+generateBtn.addEventListener("click", updatePassword);
+
+darkModeBtn.addEventListener("click", toggleDarkMode);
+lightModeBtn.addEventListener("click", toggleLightMode);
+
+lightModeBtn.style.display = "none";
